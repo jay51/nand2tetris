cd740e4af8b9ca8cb3241f202b585626ff282dce
jay51 <neneojaafar3@gmail.com>
2020-03-06 00:00:39 -0600

    refactor to class

--- 08/vm-translator/python-vm-translator/vm-translator.py
+++ 08/vm-translator/python-vm-translator/vm-translator.py
@@ -33,7 +33,6 @@ class Parser:
             self.files = [path[0] + f for f in os.walk(path[0]).__next__()[2] if f.endswith(".vm")]
             self.file_name =  path[0] + path[0].replace("/", ".asm")
 
-
         self.lines = self.parse(self.files)
             
 
@@ -51,101 +50,146 @@ class Parser:
         return lines
             
 
-    def assemble_file(self, func, f_name=None):
-        file_name = f_name if f_name else self.file_name
-        with open(file_name, "w") as f:
-            for line in self.lines:
-                f.write(func(line))
-        
-        print("Wrote: %d VM instructions to %s" %(len(self.lines), file_name))
+    def get_new_filename(self):
+        return self.file_name
+
+    def get_parsed_lines(self):
+        return self.lines
 
 
 
-def vm_translator(line): 
-    
-    print(line)
-    translated_line = ""
-    random = str(uuid.uuid1())
+class Translator():
 
-    try:
-        if line[0].upper() == "PUSH":
-            segment = line[1]
-            position = int(line[2]) + 5 if segment == "temp" else line[2]
-            position = int(line[2]) + 16 if segment == "static" else position
+    def __init__(self, parser):
+        self.parser = parser
+        self.lines = parser.get_parsed_lines()
+        self.file_name = parser.get_new_filename()
 
-            translated_line = push_instruction[segment].replace("{random}", random)
-            translated_line = translated_line.replace("{position}", str(position))
-            this_that = "THIS" if line[2] == "0" else "THAT" 
-            translated_line = translated_line.replace("{THIS_THAT}", this_that)
+        self.num_of_function_calls = 0
+        self.inside_function = False
+        self.curr_function = ""
 
-        elif line[0].upper() == "POP":
-            segment = line[1]
-            position = int(line[2]) + 5 if segment == "temp" else line[2]
-            position = int(line[2]) + 16 if segment == "static" else position
 
-            translated_line = pop_instruction[segment].replace("{random}", random)
-            translated_line = translated_line.replace("{position}", str(position))
-            this_that = "THIS" if line[2] == "0" else "THAT" 
-            translated_line = translated_line.replace("{THIS_THAT}", this_that)
+    def push_instruction(self, line, random):
+        segment = line[1]
+        position = int(line[2]) + 5 if segment == "temp" else line[2]
+        position = int(line[2]) + 16 if segment == "static" else position
 
-        elif line[0].upper() == "LABEL":
-            label_name = line[1]
-            translated_line = branching_instruction["label"].replace("{label-name}", label_name)
-            
-        elif line[0].upper() == "GOTO":
-            label_name = line[1]
-            translated_line = branching_instruction["goto"].replace("{label-name}", label_name)
+        translated_line = push_instruction[segment].replace("{random}", random)
+        translated_line = translated_line.replace("{position}", str(position))
+        this_that = "THIS" if line[2] == "0" else "THAT"
+        translated_line = translated_line.replace("{THIS_THAT}", this_that)
+        return translated_line
 
-        elif line[0].upper() == "IF-GOTO":
-            label_name = line[1]
-            translated_line = branching_instruction["if-goto"].replace("{label-name}", label_name)
+    def pop_instruction(self, line, random):
+        segment = line[1]
+        position = int(line[2]) + 5 if segment == "temp" else line[2]
+        position = int(line[2]) + 16 if segment == "static" else position
 
-        elif line[0].upper() == "FUNCTION":
+        translated_line = pop_instruction[segment].replace("{random}", random)
+        translated_line = translated_line.replace("{position}", str(position))
+        this_that = "THIS" if line[2] == "0" else "THAT"
+        translated_line = translated_line.replace("{THIS_THAT}", this_that)
+        return translated_line
+
+
+    def define_label(self, line):
+        label_name = line[1] if not self.inside_function else line[1] + "$" + self.curr_function
+        translated_line = branching_instruction["label"].replace("{label-name}", label_name)
+        return translated_line
+
+
+    def define_goto(self, line):
+        label_name = line[1] if not self.inside_function else line[1] + "$" + self.curr_function
+        translated_line = branching_instruction["goto"].replace("{label-name}", label_name)
+        return translated_line
+
+
+    def define_if_goto(self, line):
+        label_name = line[1] if not self.inside_function else line[1] + "$" + self.curr_function
+        translated_line = branching_instruction["if-goto"].replace("{label-name}", label_name)
+        return translated_line
+
+
+    def define_function(self, line):
             function_name = line[1]
             num_locals = int(line[2])
-            if(num_locals):
-                push_zeros_to_stack = """
-                @SP
-                A=M
-                M=0
-                @SP
-                M=M+1
-                """
-            else:
-                push_zeros_to_stack = ""
 
-            for _ in range(1, num_locals):
-                push_zeros_to_stack+= push_zeros_to_stack
+            init_locals = ""
+            if(num_locals):
+                for _ in range(1, num_locals):
+                    init_locals += self.push_instruction(["push", "constant", "0"], "0")
 
             translated_line = function_instruction["function"].replace("{function-name}", function_name)
-            translated_line = translated_line.replace("{N-LCL}", push_zeros_to_stack)
-
-        elif line[0].upper() == "CALL":
-            function_name = line[0]
-            num_of_calls=0
-            return_address = line[0] + "-RET-" + str(num_of_calls)
-            num_of_calls += 1
-            num_args = line[2]
-
-            translated_line = function_instruction["call"].replace("{return-address}", return_address)
-            translated_line = translated_line.replace("{function-name}", function_name)
-            translated_line = translated_line.replace("{num-args}", num_args)
+            translated_line = translated_line.replace("{N-LCL}", init_locals)
+            return translated_line
 
 
-        elif line[0].upper() == "RETURN":
-            translated_line = function_instruction["return"]
+    def call_function(self, line):
+        function_name = line[0]
+        return_address = line[0] + "-RET-" + str(self.num_of_function_calls)
+        self.num_of_function_calls += 1
+        num_args = line[2]
+
+        translated_line = function_instruction["call"].replace("{return-address}", return_address)
+        translated_line = translated_line.replace("{function-name}", function_name)
+        translated_line = translated_line.replace("{num-args}", num_args)
+        return translated_line
 
 
-        else:
-            translated_line = arithmetic_ops[line[0]].replace("{random}", random)
+    def call_return(self, line):
+        return function_instruction["return"]
 
-    except Exception as e:
-        # print(e)
-        traceback.print_exc()
-        sys.stderr.write("Error: Wrong instruction %s\n" %line[0])
-        sys.exit()
 
-    return translated_line
+    def translate(self):
+        with open(self.file_name, "w") as f:
+            for line in self.lines:
+                translation = self.translate_line(line)
+                f.write(translation)
+
+        print("Wrote: %d VM instructions to %s" %(len(self.lines), self.file_name))
+
+
+    def translate_line(self, line):
+        print(line)
+        random = str(uuid.uuid1())
+
+        try:
+            if line[0].upper() == "PUSH":
+                return self.push_instruction(line, random)
+
+            if line[0].upper() == "POP":
+                return self.pop_instruction(line, random)
+
+            if line[0].upper() == "LABEL":
+                return self.define_label(line)
+
+            if line[0].upper() == "GOTO":
+                return self.define_goto(line)
+
+            if line[0].upper() == "IF-GOTO":
+                return self.define_if_goto(line)
+
+            if line[0].upper() == "FUNCTION":
+                # not necessary b/c before executing a function we need to call it
+                # self.curr_function = line[1]
+                # self.inside_function = True
+                return self.define_function(line)
+
+            if line[0].upper() == "CALL":
+                self.curr_function = line[1]
+                self.inside_function = True
+                return self.call_function(line)
+
+            if line[0].upper() == "RETURN":
+                return self.call_return(line)
+
+            return arithmetic_ops[line[0]].replace("{random}", random)
+
+        except Exception as e:
+            traceback.print_exc()
+            sys.stderr.write("Error: Wrong instruction %s\n" %line[0])
+            sys.exit()
 
 
 
@@ -154,7 +198,8 @@ def main():
         sys.stderr.write("Usage: vm-translator <file_name.vm>\n\tvm-translator <dir_name>/\n")
         sys.exit(2)
 
-    Parser(sys.argv[1:]).assemble_file(vm_translator)
+    parser = Parser(sys.argv[1:])
+    Translator(parser).translate()
 
 
 if __name__ == "__main__":
